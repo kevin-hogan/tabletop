6ef9747cb0ea42ee3df3cb875f160fa6cca23486
Kevin <kevin.hogan1992@gmail.com>
2021-02-28 03:45:07 +0000

    persistent board states!

--- run_dev_server.py
+++ run_dev_server.py
@@ -1,46 +1,47 @@
 import os
 import random
 import string
+import pymongo
 from flask import Flask, send_from_directory, request, jsonify, redirect
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from flask_cors import CORS
 
 app = Flask(__name__, static_folder="frontend/build")
-app.config['SECRET_KEY'] = 'secret!'
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 PLAYER_POSITIONS_KEY = 'playerPositions'
 BOARD_COLORS_KEY = 'boardColors'
 ROOM_KEY = 'room'
-board_states = {}
+
+db_client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = db_client['tabletop']
+db_board_states = db['board_states']
 
 @socketio.on('join')
 def on_join(data):
     room = data[ROOM_KEY]
     join_room(room)
-    emit(PLAYER_POSITIONS_KEY, board_states[room][PLAYER_POSITIONS_KEY], room=request.sid)
-    emit(BOARD_COLORS_KEY, board_states[room][BOARD_COLORS_KEY], room=request.sid)
+    board_state = db_board_states.find_one({ROOM_KEY: room})
+    emit(PLAYER_POSITIONS_KEY, board_state[PLAYER_POSITIONS_KEY], room=request.sid)
+    emit(BOARD_COLORS_KEY, board_state[BOARD_COLORS_KEY], room=request.sid)
 
 @socketio.on('updatePlayerPositions')
 def update_player_positions(player_position_update):
-    global board_states
     room = player_position_update[ROOM_KEY]
-    board_states[room][PLAYER_POSITIONS_KEY] = player_position_update[PLAYER_POSITIONS_KEY]
+    db_board_states.update_one({ROOM_KEY: room}, { "$set": { PLAYER_POSITIONS_KEY: player_position_update[PLAYER_POSITIONS_KEY]} })
     emit(PLAYER_POSITIONS_KEY, player_position_update[PLAYER_POSITIONS_KEY], room=room)
 
 @socketio.on('updateBoardColors')
 def update_board_colors(board_colors_update):
-    global board_states
     room = board_colors_update[ROOM_KEY]
-    board_states[room][BOARD_COLORS_KEY] = board_colors_update[BOARD_COLORS_KEY]
+    db_board_states.update_one({ROOM_KEY: room}, { "$set": { BOARD_COLORS_KEY: board_colors_update[BOARD_COLORS_KEY]} })
     emit(BOARD_COLORS_KEY, board_colors_update[BOARD_COLORS_KEY], room=room)
 
 @app.route('/room', methods=['POST'])
 def create_room():
-    global board_states
     room = ''.join(random.choices(string.ascii_lowercase + string.digits, k=20))
-    board_states[room] = {PLAYER_POSITIONS_KEY: {}, BOARD_COLORS_KEY: {}}
+    db_board_states.insert_one({ROOM_KEY: room, PLAYER_POSITIONS_KEY: {}, BOARD_COLORS_KEY: {}})
     return jsonify({ROOM_KEY: room})
 
 
--- ubuntu_dev_setup.sh
+++ ubuntu_dev_setup.sh
@@ -1,3 +1,10 @@
+# Install MongoDB
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+
 sudo apt update && sudo apt install -y python3-venv python3-pip
 pip3 install -r requirements.txt
 pip3 install pylint
